2d3c93ea09e68e7fc67e5fd191191a3e8e2bd910
Jason Ola <olajason@yahoo.fr>
2022-06-09 16:13:41 +0200

    cards

--- index.js
+++ index.js
@@ -10,14 +10,45 @@ d3.tsv("valeurs_nutritives.tsv", function(d){
 
     }
 }).then(donnees => {
-    let show_img = function(index){
-        let img = document.createElement("img")
-        img.src = donnees[index].image
-        img.width = 300
-        img.height = 300
-        document.body.appendChild(img)
+    // let show_img = function(index){
+    //     let div = document.createElement("div")
+    //     let img = document.createElement("img")
+    //     img.src = donnees[index].image
+    //     img.width = 300
+    //     img.height = 300
+    //     console.log(choiceWrapper)
+    //     choiceWrapper.appendChild(div)
+    //     div.appendChild(img)
+    // }
+
+    let choiceWrapper = document.querySelector(".choiceWrapper")
+
+    let show_mult_img = function(array){
+        //choiceWrapper.removeChild("img")
+        for (let i = 0; i < array.length; i++) {
+            //show_img(array[i])
+            let div = document.createElement("div")
+            div.setAttribute("class","card")
+            let img = document.createElement("img")
+            img.src = donnees[array[i]].image
+            img.width = 300
+            img.height = 300
+            choiceWrapper.appendChild(div)
+            div.appendChild(img)    
+        }
     }
+    function randomThreeNum(min, max) { 
+        let n = []; 
+        for(let i=0;i<3;i++){ 
+        n.push(Math.floor(Math.random() * max) + min); 
+        } 
+        return n; 
+        } 
     
-    show_img(0)
-    show_img(1)
+    let button = document.createElement("button")
+    button.addEventListener("click", function(){
+        //choiceWrapper.removeChild("div")
+        show_mult_img(randomThreeNum(0,donnees.length))})
+    document.body.appendChild(button)
+  
 })
